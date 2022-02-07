9192205eefcf3daf939d765ba7807fadbec66b45
Luke Bracegirdle <l.bracegirdle@vhshed.com>
2022-02-07 17:13:26 +0000

    Avoid use of State

--- Sources/JWLUIFramework/JWLUIFramework.swift
+++ Sources/JWLUIFramework/JWLUIFramework.swift
@@ -3,21 +3,21 @@ import SwiftUI
 public struct JWLUIFramework {
     public struct MainTextField: View {
         @State var placeholder: String
-        @Binding var text: String
+        var text: String
         
         /// This is a text field
         /// - Parameters:
         ///   - placeholder: the text you see before you type anything
         ///   - text: the text that gets typed into the field
-        public init(placeholder: String, text: Binding<String>) {
+        public init(placeholder: String, text: String) {
             self._placeholder = State(initialValue: placeholder)
-            self._text = text
+            self.text = text
         }
     
         public var body: some View {
             HStack {
                 Image(systemName: "person").foregroundColor(Color.blue)
-                TextField(placeholder, text: $text)
+                TextField(placeholder, text: .constant("test value"))
                     .font(.system(size: 20, weight: .bold, design: .default))
                     .foregroundColor(Color.blue)
             }.padding()
