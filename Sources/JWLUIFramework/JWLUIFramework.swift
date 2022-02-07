dae5216ffd865f01e6b1c8871e9e528bbbefe04a
Luke Bracegirdle <l.bracegirdle@vhshed.com>
2022-02-07 15:48:26 +0000

    Add docs

--- Sources/JWLUIFramework/JWLUIFramework.swift
+++ Sources/JWLUIFramework/JWLUIFramework.swift
@@ -5,20 +5,24 @@ public struct JWLUIFramework {
         @State var placeholder: String
         @Binding var text: String
         
+        /// This is a text field
+        /// - Parameters:
+        ///   - placeholder: the text you see before you type anything
+        ///   - text: the text that gets typed into the field
         public init(placeholder: String, text: Binding<String>) {
             self._placeholder = State(initialValue: placeholder)
             self._text = text
         }
     
-    public var body: some View {
-        HStack {
-            Image(systemName: "person").foregroundColor(Color.blue)
-            TextField(placeholder, text: $text)
-                .font(.system(size: 20, weight: .bold, design: .default))
-                .foregroundColor(Color.blue)
-        }.padding()
-        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 2))
-        
-    }
+        public var body: some View {
+            HStack {
+                Image(systemName: "person").foregroundColor(Color.blue)
+                TextField(placeholder, text: $text)
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    .foregroundColor(Color.blue)
+            }.padding()
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 2))
+            
+        }
     }
 }
